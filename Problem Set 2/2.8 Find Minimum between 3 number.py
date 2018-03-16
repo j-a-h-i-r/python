54fcbc32804c38e37540caf3647c90ebdc5fb34d
Jahir Islam <jahirtheboss@gmail.com>
2018-03-16 11:23:09 +0600

    Create 2.8 Find Minimum between 3 number.py

---
+++ Problem Set 2/2.8 Find Minimum between 3 number.py
@@ -0,0 +1,7 @@
+number1 = input("Input First number:")
+number2 = input("Input Second number:")
+number3 = input("Input Third Number:")
+
+minimum = min(number1, number2, number3)
+
+print("Minimum:", minimum)
